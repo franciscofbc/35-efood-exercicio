a398f8c3bd892ff4a09a1d94bb36619cd94935d6
Francisco Berti da Cruz <fbc991@live.com>
2023-05-18 11:52:29 -0300

    fazendo modal

--- src/components/OptionRestaurantItem/index.tsx
+++ src/components/OptionRestaurantItem/index.tsx
@@ -1,4 +1,14 @@
-import { Card, Description, Title } from './style'
+import { useState } from 'react'
+import {
+  Card,
+  Description,
+  FotoCardapio,
+  ImgFechar,
+  Modal,
+  ModalContent,
+  Title
+} from './style'
+import close from '../../assets/images/close.png'
 
 type Props = {
   foto: string
@@ -7,13 +17,34 @@ type Props = {
 }
 
 const OptionRestaurantItem = ({ foto, nome, descricao }: Props) => {
+  const [isVisible, setIsVisible] = useState(false)
+
   return (
-    <Card>
-      <img src={foto} alt={nome} />
-      <Title>{nome}</Title>
-      <Description>{descricao}</Description>
-      <a href="#">Adicionar ao carrinho</a>
-    </Card>
+    <>
+      <Card>
+        <img src={foto} alt={nome} />
+        <Title>{nome}</Title>
+        <Description>{descricao}</Description>
+        <a onClick={() => setIsVisible(true)}>Mais detalhes</a>
+      </Card>
+      {isVisible && (
+        <Modal>
+          <ModalContent className="container">
+            <FotoCardapio src={foto} alt={nome} />
+            <div>
+              <h2>{nome}</h2>
+              <p>{descricao}</p>
+              <a href="">Adcionar ao carrinho</a>
+            </div>
+            <ImgFechar
+              src={close}
+              alt="Fechar"
+              onClick={() => setIsVisible(false)}
+            />
+          </ModalContent>
+        </Modal>
+      )}
+    </>
   )
 }
 
--- src/components/OptionRestaurantItem/style.ts
+++ src/components/OptionRestaurantItem/style.ts
@@ -15,6 +15,7 @@ export const Card = styled.div`
     padding: 4px;
     font-size: 14px;
     font-weight: bold;
+    cursor: pointer;
   }
 
   > img {
@@ -30,10 +31,71 @@ export const Title = styled.h2`
   font-weight: bold;
   font-size: 16px;
 `
+
 export const Description = styled.p`
   color: ${colors.pinkFooter};
   font-weight: 400;
   font-size: 14px;
   line-height: 22px;
   margin-bottom: 8px;
+`
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  // left: 0;
+  // right: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ModalContent = styled.div`
+  display: flex;
+  // align-items: center;
+  padding: 32px;
+  background-color: ${colors.pink};
+  position: relative; //por causa da ImgFechar
+  width: 75%;
+
+  h2 {
+    color: ${colors.pinkFooter};
+    font-weight: 900;
+    font-size: 18px;
+    margin-bottom: 16px;
+  }
+
+  p {
+    color: ${colors.pinkFooter};
+    font-weight: 400;
+    font-size: 14px;
+    margin-bottom: 16px;
+    line-height: 24px;
+  }
+
+  a {
+    color: ${colors.pink};
+    background-color: ${colors.pinkFooter};
+    text-decoration: none;
+    // display: block;
+    text-align: center;
+    padding: 4px 8px; //mudou
+    font-size: 14px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+`
+export const FotoCardapio = styled.img`
+  height: 280px;
+  width: 280px;
+  margin-right: 24px;
+`
+
+export const ImgFechar = styled.img`
+  cursor: pointer;
+  position: absolute;
+  top: 8px;
+  right: 8px;
 `
